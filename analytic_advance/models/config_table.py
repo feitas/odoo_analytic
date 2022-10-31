9f5bbebbac9d53e4498cc67a8eaae8a5cd7362b7
f043 <feitas2022@qq.com>
2022-10-31 19:40:10 +0800

    优化中间配置表

--- analytic_advance/models/config_table.py
+++ analytic_advance/models/config_table.py
@@ -10,3 +10,9 @@ class ConfigTable(models.Model):
     model_name = fields.Many2one('ir.model',string="系统模型")
     field_name = fields.Many2one('ir.model.fields',string="系统字段",domain="[('model_id','=',model_name)]")
     name = fields.Char(string="自定义字段名称")
+
+    @api.onchange("field_name")
+    def _onchange_field_name(self):
+        for recond in self:
+            if recond.model_name and recond.field_name:
+                recond.name = recond.model_name.name + '-' + recond.field_name.name
