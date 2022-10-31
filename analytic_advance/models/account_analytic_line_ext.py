54a4433c7a23864847dbb70a5a35428ec9c228d4
hanlin <hanlin@git.com>
2022-10-31 10:14:51 +0800

    [Fix]-操作数优化，操作数1取消固定，可选择；增加日志验证

--- analytic_advance/models/account_analytic_line_ext.py
+++ analytic_advance/models/account_analytic_line_ext.py
@@ -21,13 +21,21 @@ class AccountAnalyticLineExt(models.Model):
                 return arg1 / arg2 if arg2 else 0
             return 0
 
-        if 'account_id' in vals and 'move_id' in vals and vals.get('account_id') and vals.get('account_id'):
-            arg1 = self.env["account.move.line"].sudo().browse(vals.get('move_id')).price_unit
-            defaults = self.env["account.analytic.default"].sudo().search([('analytic_id.id', '=', vals['account_id']),('first_model.model', '=', 'product.product')])
-            if defaults and defaults[0].second_model and defaults[0].second_field_name:
-                second_model = defaults[0].second_model.model
-                second_field_name = defaults[0].second_field_name
+        if 'account_id' in vals and vals.get('account_id'):
+            defaults = self.env["account.analytic.default"].sudo().search([('analytic_id.id', '=', vals['account_id'])])
+            rule = defaults and defaults[0]
+            if rule and rule.first_model and rule.first_field_name and rule.second_model and rule.second_field_name and rule.operator:
+                first_model = rule.first_model.model
+                first_field_name = rule.first_field_name
+                arg1 = sum(self.env[first_model].sudo().search([]).mapped(first_field_name))
+                second_model = rule.second_model.model
+                second_field_name = rule.second_field_name
                 arg2 = sum(self.env[second_model].sudo().search([]).mapped(second_field_name))
-                amount = exec_operator(defaults[0].operator, arg1, arg2)
+                amount = exec_operator(rule.operator, arg1, arg2)
                 vals["amount"] = amount
+                _logger.warning("操作数1：{}".format(arg1))
+                _logger.warning("操作数2：{}".format(arg2))
+            else:
+                vals["amount"] = 0
+                _logger.warning("找不到对应规则或规则中缺少参数！")
         return super(AccountAnalyticLineExt, self).create(vals)
